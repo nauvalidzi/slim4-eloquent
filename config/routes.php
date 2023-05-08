9fe9f093712e470547486bfbc7017d22c705e1dd
Nauval Hafidzi <nauval.hafidzi@gmail.com>
2023-05-08 12:26:30 +0700

    Add Database

--- config/routes.php
+++ config/routes.php
@@ -4,7 +4,6 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Controllers\TodoListController;
 use Slim\App;
-// use Slim\Exception\HttpNotFoundException;
 
 return function (App $app) {
     $app->options('/{routes:.+}', function ($request, $response, $args) {
@@ -29,12 +28,4 @@ return function (App $app) {
     $app->post('/todo', [TodoListController::class, 'store']);
     $app->patch('/todo/{id}', [TodoListController::class, 'update']);
     $app->delete('/todo/{id}', [TodoListController::class, 'destroy']);
-
-    // /**
-    //  * Catch-all route to serve a 404 Not Found page if none of the routes match
-    //  * NOTE: make sure this route is defined last
-    //  */
-    // $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
-    //     throw new HttpNotFoundException($request);
-    // });
 };
